065d94d9d8da259404187ae33a8402d9279b315e
Paulo André <andrepaulos.2001@gmail.com>
2023-11-15 13:24:12 -0300

    Teste de conexão em PostgreSql (Failed)
    Tentei conectar o java com o banco de teste em Postgre e não deu certo.

--- BarberPro/testeConect/src/main/java/Dao/ConnectionDAO.java
+++ BarberPro/testeConect/src/main/java/Dao/ConnectionDAO.java
@@ -13,15 +13,20 @@ public class ConnectionDAO {
     public ConnectionDAO(){
         
     }
-    
+    // "org.postgresql:postgresql:42.6.0"
     public static Connection conn = null;
     
     public static Connection getConnection(){
+        
         try {
             if(conn == null){
-                Class.forName("com.mysql.jdbc.Driver"); /* Aqui registra */
+                
+                conn = DriverManager.getConnection("jdbc:postgresql//localhost:5433/teste");
+                
+                /* Conexao para MySQL
                 // jbdc:mysql:// é o driver de conexão. Guardamos o endereco do banco e o user
                 conn = DriverManager.getConnection("jbdc:mysql://localhost:3306/barber", "root", "Playdark@0712");
+                */
             }else{
                 return conn; // Se a conexao ja tiver sido criado.
             }
