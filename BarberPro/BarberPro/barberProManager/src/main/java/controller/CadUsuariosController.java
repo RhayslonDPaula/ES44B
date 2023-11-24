ed83641e1115bff0f602f28493ed0c0974743fa0
Paulo André <andrepaulos.2001@gmail.com>
2023-11-23 23:36:20 -0300

    Criacao de Controller e Helper para a view Cadastro de usuarios

--- BarberPro/BarberPro/barberProManager/src/main/java/controller/CadUsuariosController.java
+++ BarberPro/BarberPro/barberProManager/src/main/java/controller/CadUsuariosController.java
@@ -4,16 +4,44 @@
  */
 package controller;
 import View.CadastroUsuarios;
-
+import Model.Funcionario;
+import dao.FuncionarioDAO;
+import controller.helper.CadUsuariosHelper;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  * @author paulo
  */
 public class CadUsuariosController {
     
-    CadastroUsuarios view;
+    FuncionarioDAO funcionarioDao = new FuncionarioDAO();
+    
+    private CadUsuariosHelper helper;
+    private CadastroUsuarios cadUserView;
+    int confirma = 0;
     
     public CadUsuariosController(CadastroUsuarios view){
-        this.view = view;
+        cadUserView = view;
+        helper = new CadUsuariosHelper(view);
+    }
+     
+    
+    public void adicionaFunc(){
+        Funcionario func = new Funcionario();
+        
+        func = helper.obtemModelo(); // recebe o modelo
+        
+        try {
+            if(func.getEmail() == null || func.getSenha() == null || func.getFun_cargo() == null || func.getFun_nome() == null){
+                cadUserView.exibeMensagem("Por favor, preencha ao menos os campos obrigatorios!");
+            } else {
+                funcionarioDao.insertFuncionario(func); 
+                cadUserView.exibeMensagem("Funcionario Adicionado com sucesso!");
+            }
+        } catch (SQLException ex) {
+            cadUserView.exibeMensagem("Erro ao adicionar funcionario");
+        }
     }
     
 }
