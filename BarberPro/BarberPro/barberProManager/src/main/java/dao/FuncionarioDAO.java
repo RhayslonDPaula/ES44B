64bb4c0f69fccd4142209f3fc42550b1b3ff4a9e
Paulo André <andrepaulos.2001@gmail.com>
2023-11-23 23:39:54 -0300

    Criacao de classe FuncionarioDAO para se comunicar com o banco
    A classe FuncionarioDAO possui métodos para se comunicar com o banco, servindo de intermedio para as ações no banco referentes à model Funcionario.

--- BarberPro/BarberPro/barberProManager/src/main/java/dao/FuncionarioDAO.java
+++ BarberPro/BarberPro/barberProManager/src/main/java/dao/FuncionarioDAO.java
@@ -4,7 +4,6 @@
  */
 package dao;
 import Model.Funcionario;
-import View.CadastroUsuarios;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,29 +15,30 @@ import java.sql.SQLException;
  */
 public class FuncionarioDAO {
     
-    private final ConnectionDAO connection = new ConnectionDAO();
-    private final CadastroUsuarios cadUser = CadastroUsuarios.getCadastroUsuarios();
-    
+    // constructor
     public FuncionarioDAO(){
     }
     
     public void insertFuncionario(Funcionario fun) throws SQLException{
         Funcionario func = fun;
         
+        /**
+         * PROBLEMA AQUI NO INSERT!!!
+         */
+        
         Connection conn;
-        conn = connection.getConnection(); // recebe a conexo
+        conn = ConnectionDAO.getConnection(); // recebe a conexo
         
         // prepara a string sql
-            String sql = "INSERT INTO(fun_nome, fun_cargo, fun_nivelAcesso, fun_senha, fun_nascimento, fun_cidade, fun_endereco) "
-                    + "VALUES('" + func.getFun_nome() + "', '" + func.getFun_cargo() + "', " + func.getFun_nvlAcesso() 
-                    + ", '" + func.getSenha() + "', '" + func.getNascimento() + ", '" + func.getFun_Cidade() +", '" + func.getFun_Endereco() + "');";
+        String sql = "INSERT INTO(fun_nome, fun_cargo, fun_nivelAcesso, fun_senha, fun_nascimento, fun_cidade, fun_endereco) "
+                + "VALUES('" + func.getFun_nome() + "', '" + func.getFun_cargo() + "', " + func.getFun_nvlAcesso() 
+                + ", '" + func.getSenha() + "', '" + func.getNascimento() + ", '" + func.getFun_Cidade() +", '" + func.getFun_Endereco() + "');";
         
         try{
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.executeQuery();
-            
         }catch(SQLException sqe){
-            cadUser.exibeMensagem("Não foi possível adicionar o funcionario ao banco");
+            
         }
     }
     
@@ -46,17 +46,16 @@ public class FuncionarioDAO {
         Funcionario func = fun;
         
         Connection conn;
-        conn = connection.getConnection(); // recebe a conexo
+        conn = ConnectionDAO.getConnection(); // recebe a conexo
         
         // sql para excluir
-        String sql = "DELETE * FROM funcionario WHERE fun_id = '" + fun.getFun_id() + "' OR fun_email = '" + fun.getEmail() + "';";
+        String sql = "DELETE * FROM funcionario WHERE fun_id = '" + func.getFun_id() + "' OR fun_email = '" + func.getEmail() + "';";
         
         try{
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.executeQuery();
-            
         }catch(SQLException sqe){
-            cadUser.exibeMensagem("Não foi possível deletar o funcionario ao banco");
+            
         }
     }
     
@@ -64,23 +63,22 @@ public class FuncionarioDAO {
         Funcionario func = fun;
         
         Connection conn;
-        conn = connection.getConnection(); // recebe a conexo
+        conn = ConnectionDAO.getConnection(); // recebe a conexo
         
         // sql para excluir
-        String sql = "DELETE * FROM funcionario WHERE fun_id = '" + fun.getFun_id() + "' OR fun_email = '" + fun.getEmail() + "';";
-        /*
-            UPDATE funcionario
-            SET coluna1 = novo_valor_armazenado,
-                coluna2 = novo_valor_armazenado,
-                colunaN = novo_valor_armazenado
-            WHERE coluna = valor_filtro;
-        */
+        String sql = "UPDATE funcionario SET"
+                + "fun_nome = '" + func.getFun_nome() + ", fun_cargo = '" + func.getFun_cargo() + "' ,"
+                + "fun_nivelAcesso = "+ func.getFun_nvlAcesso() + ", fun_email = " + func.getEmail() + ", "
+                + "fun_senha = '" + func.getSenha() + ", fun_nascimento = '" + func.getNascimento() + ", "
+                + "fun_telefone1 = '" + func.getTelefone() +", fun_cidade = '"+ func.getFun_Cidade() +", " 
+                + "fun_endereco = '"+ func.getFun_Endereco()
+                + " WHERE fun_id = '" + fun.getFun_id() + "' OR fun_email = '" + fun.getEmail() + "';";
+        
         try{
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.executeQuery();
-            
         }catch(SQLException sqe){
-            cadUser.exibeMensagem("Não foi possível deletar o funcionario ao banco");
+            
         }
     }
     
